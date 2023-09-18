181f7cbf99074ab8de4fa9561f940a57b8029e4d
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-18 23:11:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(travel){
   // Write your code here!
+  if( travel <= 800 ){
+    return ''Standard Price''
+  }
+  else if (travel <= 5000){
+    return 'Bookings to be made early.'
+  }
+  else if (travel > 5000){
+    return 'Hiked prices.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(City){
   // Write your code here!
+  if (City === 'Kisumu'){
+    return 'yes please!.'
+  }else{
+    return 'No thank you!.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'client tips'){
+    return 'Thank you so much.'
+  }else if(tip === 'no client tips'){
+    return 'Thank you! Welcome again!.'
+  }else{
+    return 'BHave a good day.'
+  }
 }
